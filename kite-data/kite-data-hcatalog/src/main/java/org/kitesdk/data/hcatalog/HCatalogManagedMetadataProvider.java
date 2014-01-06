7fd4cbe8560c6dc38c165854cdb3111569457055
Nicolas Maillard <nicolas.maillard@fifty-five.com>
2014-01-06 19:09:03 +0100

    namespace patch

--- kite-data/kite-data-hcatalog/src/main/java/org/kitesdk/data/hcatalog/HCatalogManagedMetadataProvider.java
+++ kite-data/kite-data-hcatalog/src/main/java/org/kitesdk/data/hcatalog/HCatalogManagedMetadataProvider.java
@@ -37,8 +37,15 @@ class HCatalogManagedMetadataProvider extends HCatalogMetadataProvider {
   @Override
   public DatasetDescriptor load(String name) {
     Preconditions.checkArgument(name != null, "Name cannot be null");
-
-    final Table table = hcat.getTable(HiveUtils.DEFAULT_DB, name);
+    
+    Table table = null;
+    String[]  namespace = name.split(".");
+    if(namespace.size()>1){
+	table = hcat.getTable( namespace[0], namespace[1]);
+    }else{
+	table = hcat.getTable(HiveUtils.DEFAULT_DB, name);
+    }
+    i//final Table table = hcat.getTable(HiveUtils.DEFAULT_DB, name);
 
     if (!TableType.MANAGED_TABLE.equals(table.getTableType())) {
       throw new MetadataProviderException("Table is not managed");
@@ -52,23 +59,28 @@ class HCatalogManagedMetadataProvider extends HCatalogMetadataProvider {
     Preconditions.checkArgument(name != null, "Name cannot be null");
     Preconditions.checkArgument(descriptor != null,
         "Descriptor cannot be null");
-
-    if (exists(name)) {
+    String db = HiveUtils.DEFAULT_DB;
+    String[]  namespace = name.split(".");
+    if(namespace.size()>1){
+	db = namespace[0];
+        name = namespace[1];
+    }
+    if (exists(db,name)) {
       throw new DatasetExistsException(
-          "Metadata already exists for dataset:" + name);
+          "Metadata already exists for dataset:"+db+"."+ name);
     }
 
-    logger.info("Creating a managed Hive table named: " + name);
+    logger.info("Creating a managed Hive table named: "+db+"."+ name);
 
     // construct the table metadata from a descriptor
     final Table table = HiveUtils.tableForDescriptor(
-        name, descriptor, false /* managed table */);
+        name, db,descriptor, false /* managed table */);
 
     // create it
     hcat.createTable(table);
 
     // load the created table to get the data location
-    final Table newTable = hcat.getTable(HiveUtils.DEFAULT_DB, name);
+    final Table newTable = hcat.getTable(db, name);
 
     return new DatasetDescriptor.Builder(descriptor)
         .location(newTable.getDataLocation())
